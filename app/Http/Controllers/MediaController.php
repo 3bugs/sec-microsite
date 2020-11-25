f9948d1db9bc247164a6f27d1d684b101be03210
Promlert Lovichit <promlert@gmail.com>
2020-11-25 10:53:40 +0700

    front: fix youtube video tag replace in media controller

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -29,7 +29,8 @@ class MediaController extends Controller
     $pattern = '#<figure class="media"><oembed url="(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?"></oembed></figure>#U';
     $media->content = preg_replace(
       $pattern,
-      '<iframe src="https://www.youtube.com/embed/$1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      '<div class="col-12 wrap_video"><div><iframe src="https://www.youtube.com/embed/$1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div></div>',
       $media->content
     );
 
