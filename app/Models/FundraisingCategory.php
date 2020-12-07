09cc906bcf3fda53ad569dc871e6139a1358d489
Promlert Lovichit <promlert@gmail.com>
2020-12-07 17:51:22 +0700

    add sort_index field in media

--- app/Models/FundraisingCategory.php
+++ app/Models/FundraisingCategory.php
@@ -9,7 +9,7 @@ class FundraisingCategory extends Model
 {
   use HasFactory;
 
-  protected $casts = ['id' => 'integer', 'published' => 'integer'];
+  protected $casts = ['id' => 'integer', 'published' => 'integer', 'sort_index' => 'integer'];
 
   public function fundraisings()
   {
