98c539612fd2c7965d38402ee720a554aeafd3f5
Promlert Lovichit <promlert@gmail.com>
2020-12-14 12:17:46 +0700

    connect azure/.3

--- config/database.php
+++ config/database.php
@@ -145,7 +145,7 @@ return [
             'sslmode' => 'prefer',
         ],
 
-        'sqlsrv' => [
+        'sqlsrv' => env('AZURE') == '1' ? [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
             'host' => $server, //env('DB_HOST', 'localhost'),
@@ -156,6 +156,17 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+        ] : [
+          'driver' => 'sqlsrv',
+          'url' => env('DATABASE_URL'),
+          'host' => env('DB_HOST', 'localhost'),
+          'port' => env('DB_PORT', '1433'),
+          'database' => env('DB_DATABASE', 'forge'),
+          'username' => env('DB_USERNAME', 'forge'),
+          'password' => env('DB_PASSWORD', ''),
+          'charset' => 'utf8',
+          'prefix' => '',
+          'prefix_indexes' => true,
         ],
     ],
 
