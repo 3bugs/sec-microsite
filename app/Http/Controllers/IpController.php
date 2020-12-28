04013f4130eba3b5652c1f347ab9f95048b9df21
Promlert Lovichit <promlert@gmail.com>
2020-12-29 01:08:40 +0700

    fix ip address in survey result

--- app/Http/Controllers/IpController.php
+++ app/Http/Controllers/IpController.php
@@ -13,10 +13,26 @@ class IpController extends Controller
   public function index(Request $request)
   {
     //$ip = \Request::ip();
-    $ip = request()->ip();
+    //$ip = request()->ip();
+    $ip = $this->getIp();
 
     return view('test-ip', [
       'ip' => $ip,
     ]);
+  }
+
+  public function getIp()
+  {
+    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
+      if (array_key_exists($key, $_SERVER) === true) {
+        foreach (explode(',', $_SERVER[$key]) as $ip) {
+          $ip = trim($ip); // just to be safe
+          if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false) {
+            return $ip;
+          }
+        }
+      }
+    }
+    return request()->ip(); // it will return server ip when no client ip found
   }
 }
