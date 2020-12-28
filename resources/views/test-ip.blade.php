9d7ddd7a0ba934527c5c64152fa5b16f960b303f
Promlert Lovichit <promlert@gmail.com>
2020-12-29 00:41:05 +0700

    fix ip address in survey result

--- resources/views/test-ip.blade.php
+++ resources/views/test-ip.blade.php
@@ -16,11 +16,47 @@
       justify-content: center;
       align-items: center;
     }
+
+    h1 > span {
+      color: blue;
+      font-family: monospace;
+      font-size: 12rem;
+    }
+
+    @media (max-width: 1400px) {
+      h1 > span {
+        font-size: 10rem;
+      }
+    }
+
+    @media (max-width: 1000px) {
+      h1 > span {
+        font-size: 8rem;
+      }
+    }
+
+    @media (max-width: 800px) {
+      h1 > span {
+        font-size: 6rem;
+      }
+    }
+
+    @media (max-width: 500px) {
+      h1 > span {
+        font-size: 4rem;
+      }
+    }
+
+    @media (max-width: 400px) {
+      h1 > span {
+        font-size: 3rem;
+      }
+    }
   </style>
 </head>
 
 <body>
 <p style="color: #aaa">{{ date("Y-m-d h:i:sa") }}</p>
-<h1><span style="color: blue; font-size: 6rem; font-family: monospace;">{{ $ip }}</span></h1>
+<h1><span>{{ $ip }}</span></h1>
 </body>
 </html>
