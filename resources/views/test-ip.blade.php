f7d585c967b0ebb534ca80ccbc425d21bece2921
Promlert Lovichit <promlert@gmail.com>
2020-12-29 00:29:20 +0700

    fix ip address in survey result

--- resources/views/test-ip.blade.php
+++ resources/views/test-ip.blade.php
@@ -5,9 +5,22 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
   <title>START TO GROW - Test IP Address</title>
   <!--  <link rel="SHORTCUT ICON" href="favicon.ico" type="image/x-icon">-->
+  <style>
+    html, body {
+      height: 100%;
+    }
+
+    body {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+  </style>
 </head>
 
 <body>
-  <p>IP address: {{ $ip }}</p>
+<p style="color: #aaa">{{ date("Y-m-d h:i:sa") }}</p>
+<h1><span style="color: blue; font-size: 10rem; font-family: monospace;">{{ $ip }}</span></h1>
 </body>
 </html>
